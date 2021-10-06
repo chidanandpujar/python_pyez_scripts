58f839057135cad566bdf784f154d12719a93c84
chidanandpujar <46497833+chidanandpujar@users.noreply.github.com>
2021-10-06 11:23:27 +0530

    Update check_pal.py

--- check_pal.py
+++ check_pal.py
@@ -3,8 +3,6 @@ def check_pal(str):
     if str == str[::-1]:
         return str
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     str_list = [ 'sat', 'bab', 'cat', 'tot']
     result =map(check_pal,str_list)
