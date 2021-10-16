bd050d9b8a152444fff614c15647826324d714f9
chidanandpujar <46497833+chidanandpujar@users.noreply.github.com>
2021-10-16 18:52:49 +0530

    Create check_class_method_override.py

---
+++ check_class_method_override.py
@@ -0,0 +1,20 @@
+class TestMethods:
+    def test1(self):
+        print("test1 is completed")
+    def test2(self):
+        print("test2 is completed")
+    def test3(self):
+        print("test3 is failed")
+
+class results(TestMethods):
+    def result(self):
+        print("Tests Completed Successfully")
+    def test3(self):
+        print("test3 is successful")
+
+
+testexec1=results()
+testexec1.test1()
+testexec1.test2()
+testexec1.result()
+testexec1.test3()
