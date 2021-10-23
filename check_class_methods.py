d4857c53680d698cbc8a6dc81b2d487ea2e9de80
chidanandpujar <46497833+chidanandpujar@users.noreply.github.com>
2021-10-23 16:39:09 +0530

    Create check_class_methods.py

---
+++ check_class_methods.py
@@ -0,0 +1,28 @@
+class cls_methods:
+    """" Demo of class methods  regular, static and class methods """
+    def regular(self):
+        print(" checking regular method")
+        print("regular methods must and should have self as argument")
+        print("regular methods can be called from instantiated objects")
+
+    @staticmethod
+    def static_method():
+        print("checking static methods")
+        print("static methods are decorated with @staticmethod")
+        print("static methods can be called with class name")
+        print("static methods can be called with instantiated object name")
+        print("static methods does not take args or params")
+
+    @classmethod
+    def class_method(cls):
+        print("checking class methods")
+        print("class methods are decorated with @classmethod")
+        print("class methods can be called from class name")
+        print("class metohds can be called from instantiated object name")
+
+
+cls1 = cls_methods()
+cls1.regular()
+cls1.static_method()
+cls1.class_method()
+
