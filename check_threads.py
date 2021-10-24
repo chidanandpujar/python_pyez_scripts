f975d65af04a6eb8705a199fb9e113c8fe6e209a
chidanandpujar <46497833+chidanandpujar@users.noreply.github.com>
2021-10-24 15:59:14 +0530

    Update check_threads.py

--- check_threads.py
+++ check_threads.py
@@ -1,7 +1,15 @@
 import threading
 
-def my_test_thread():
-    print("simple thread execution")
+def my_thread_function():
+    print("Simple thread execution")
 
-t = threading.Thread(target=my_test_thread())
+t = threading.Thread(target=my_thread_function())
 t.start()
+
+class my_thread_class(threading.Thread):
+    def run(self):
+        print("Simple thread execution with derived class threading.Thread")
+
+ct1 = my_thread_class()
+ct1.start()
+ct1.join()
